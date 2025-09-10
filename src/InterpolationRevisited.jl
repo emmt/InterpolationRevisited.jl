58909a0f4c231437828c3cd46c10bcfe7aca68a3
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2025-09-10 15:26:33 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.56

---
+++ src/InterpolationRevisited.jl
@@ -0,0 +1,5 @@
+module InterpolationRevisited
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using InterpolationRevisited
+using Test
+
+@testset "InterpolationRevisited.jl" begin
+    # Write your tests here.
+end
